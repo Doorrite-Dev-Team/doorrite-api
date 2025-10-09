ec34a60a8b6c6411330765fe57376bd237bcdb9d
abuAbdur-rahman <abdulazeezadekiilekun@gmail.com>
2025-10-09 11:39:27 +0100

    fix: email errors

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,19 +1,31 @@
-
 import "dotenv/config";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 //import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  tls: {
+    rejectUnauthorized: false, // only for debugging
+  },
+  logger: true,
+  debug: true,
 });
 
-
-const sendmail = async (to: string, subject: string, text: string, html?: string) => {
+const sendmail = async (
+  to: string,
+  subject: string,
+  text: string,
+  html?: string
+) => {
   try {
     //send email using the transporter
     const info = await transporter.sendMail({
@@ -29,6 +41,6 @@ const sendmail = async (to: string, subject: string, text: string, html?: string
     console.error("Error sending email:", error);
     throw error;
   }
-}
+};
 
 export default sendmail;
