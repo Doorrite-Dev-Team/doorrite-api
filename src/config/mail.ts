e4cef51ad2c6a637d004a4d29e4ec28b7dd7b364
abuAbdur-rahman <abdulazeezadekiilekun@gmail.com>
2025-10-12 07:04:21 +0100

    Use Proxy Instead of Resend or SMTP

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,14 @@
+import axios from 'axios';
 import "dotenv/config";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+type ProxyResponse = {
+  ok: boolean,
+  error: string
+  messageId: string
+}
 
 const sendMail = async (
   to: string,
@@ -10,21 +17,20 @@ const sendMail = async (
   html?: string
 ) => {
   try {
-    const fromEmail = process.env.RESEND_FROM ?? "doorrite.info@gmail.com";
+    // const fromEmail = process.env.RESEND_FROM ?? "onboarding@resend.dev";
 
-    const response = await resend.emails.send({
-      from: fromEmail,
+    const {data} = await axios.post<ProxyResponse>(`${process.env.PROXY_URL}/api/send-email`, {
       to,
       subject,
       text,
       html,
     });
 
-    console.log(
-      "✅ Email sent successfully:",
-      response.data?.id ?? "No ID returned"
-    );
-    return response.data;
+    if (data.error || !data.ok) {
+      throw new Error("Error Sending Email: " + data.error);
+    }
+    console.log("Raw response:", data);
+    return data;
   } catch (error: any) {
     console.error("❌ Email send failed:", error.message || error);
     throw new Error(error.message || "Email send failed");
