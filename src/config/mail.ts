326a0186d02664770549915314cdd70cdd22b0e1
abuAbdur-rahman <abdulazeezadekiilekun@gmail.com>
2025-10-12 08:33:38 +0100

    Fix Build problem

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 import "dotenv/config";
 // import { Resend } from "resend";
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
 type ProxyResponse = {
-  ok: boolean,
-  error: string
-  messageId: string
-}
+  ok: boolean;
+  error: string;
+  messageId: string;
+};
 
 const sendMail = async (
   to: string,
@@ -19,12 +19,21 @@ const sendMail = async (
   try {
     // const fromEmail = process.env.RESEND_FROM ?? "onboarding@resend.dev";
 
-    const {data} = await axios.post<ProxyResponse>(`${process.env.PROXY_URL}/api/send-email`, {
-      to,
-      subject,
-      text,
-      html,
-    });
+    const { data } = await axios.post<ProxyResponse>(
+      `${process.env.PROXY_URL}/api/send-email`,
+      {
+        to,
+        subject,
+        text,
+        html,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.PROXY_API_KEY,
+        },
+      }
+    );
 
     if (data.error || !data.ok) {
       throw new Error("Error Sending Email: " + data.error);
